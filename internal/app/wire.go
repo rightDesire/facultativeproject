64af2c44f3d080fc5b9db6ad4bdf8cd5db831882
rightDesire <14beav@gmail.com>
2025-03-27 08:41:26 +0000

    Инциализирует работу с инструментом для внедрения зависимостей Wire, логика с центральным оркестратором App

---
+++ internal/app/wire.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	"github.com/google/wire"
+	"github.com/rightDesire/facultativeproject/internal/configs"
+)
+
+func InitApp() *App {
+	wire.Build(
+		configs.NewConfigsFromEnv(),
+
+		NewApp,
+	)
+	return &App{}
+}
